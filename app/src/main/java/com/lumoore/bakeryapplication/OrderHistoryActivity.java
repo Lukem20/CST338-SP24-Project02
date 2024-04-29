4f5b2437ba357faa0f64a61eabdf4df231e60986
Luke Moore <lukemoore237@gmail.com>
2024-04-29 13:53:36 -0700

    Added intent factories.

--- app/src/main/java/com/lumoore/bakeryapplication/OrderHistoryActivity.java
+++ app/src/main/java/com/lumoore/bakeryapplication/OrderHistoryActivity.java
@@ -2,13 +2,26 @@ package com.lumoore.bakeryapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.lumoore.bakeryapplication.databinding.ActivityOrderHistoryBinding;
+
 public class OrderHistoryActivity extends AppCompatActivity {
+
+    private ActivityOrderHistoryBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityOrderHistoryBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_order_history);
+
+
+    }
+
+    public static Intent OrderHistoryIntentFactory(Context context) {
+        return new Intent(context, OrderHistoryActivity.class);
     }
 }
--- app/src/main/java/com/lumoore/bakeryapplication/UserInformationActivity.java
+++ app/src/main/java/com/lumoore/bakeryapplication/UserInformationActivity.java
@@ -2,13 +2,26 @@ package com.lumoore.bakeryapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.lumoore.bakeryapplication.databinding.ActivityUserInformationBinding;
 
 public class UserInformationActivity extends AppCompatActivity {
 
+    private ActivityUserInformationBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityUserInformationBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_user_information);
+
+
     }
+
+    public static Intent UserInformationIntentFactory(Context context) {
+        return new Intent(context, UserInformationActivity.class);
+    }
+
 }
