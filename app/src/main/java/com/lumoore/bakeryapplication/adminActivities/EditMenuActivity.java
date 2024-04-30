13d8960d73bda7a9bff5d3ad2f15aabae2e06ef9
Luke Moore <lukemoore237@gmail.com>
2024-04-30 15:22:48 -0700

    Added edit Food and Drink menu options.

--- app/src/main/java/com/lumoore/bakeryapplication/adminActivities/EditMenuActivity.java
+++ app/src/main/java/com/lumoore/bakeryapplication/adminActivities/EditMenuActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.lumoore.bakeryapplication.R;
+import com.lumoore.bakeryapplication.databinding.ActivityAdminPageBinding;
 import com.lumoore.bakeryapplication.databinding.ActivityEditMenuBinding;
 
 public class EditMenuActivity extends AppCompatActivity {
@@ -19,7 +21,27 @@ public class EditMenuActivity extends AppCompatActivity {
         binding = ActivityEditMenuBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_edit_menu);
 
+        binding.FoodMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
+
+        binding.DrinksMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        binding.goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = AdminActivity.AdminIntentFactory(getApplicationContext());
+                startActivity(intent);
+            }
+        });
     }
 
     public static Intent EditMenuIntentFactory(Context context) {
