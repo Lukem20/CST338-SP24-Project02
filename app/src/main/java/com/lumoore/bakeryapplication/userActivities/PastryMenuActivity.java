90b45f15820632ac6205a5e0abf1ee02a9371ab7
Luke Moore <lukemoore237@gmail.com>
2024-05-05 21:14:18 -0700

    Started PastryMenuActivity UI.

---
+++ app/src/main/java/com/lumoore/bakeryapplication/userActivities/PastryMenuActivity.java
@@ -0,0 +1,16 @@
+package com.lumoore.bakeryapplication.userActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lumoore.bakeryapplication.R;
+
+public class PastryMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pastry_menu);
+    }
+}
